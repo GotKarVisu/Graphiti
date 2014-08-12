c43552ad489bdbd9bd05ab6c45cd1f28d44e6067
Stechpalme <andre.karge@gmail.com>
2014-08-12 17:03:15 +0200

    java html parser hinzugefuegt

---
+++ src/wiki_parser/Parser.java
@@ -0,0 +1,57 @@
+package wiki_parser;
+import java.io.IOException;
+import java.io.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Parser {
+	public static void main(String[] args) {
+		System.out.println("test");
+		parse();
+	}
+	public static void parse() {
+		String url = "http://de.wikipedia.org/wiki/Bauhaus-Universit%C3%A4t_Weimar";
+		System.out.printf("Fetching %s...", url);
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Elements links = doc.select("a[href]");
+		Elements media = doc.select("[src]");
+		Elements imports = doc.select("link[href]");
+		for (Element src : media) {
+            if (src.tagName().equals("img"))
+                print(" * %s: <%s> %sx%s (%s)",
+                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
+                        trim(src.attr("alt"), 20));
+            else
+                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+        }
+
+        print("\nImports: (%d)", imports.size());
+        for (Element link : imports) {
+            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
+        }
+
+        print("\nLinks: (%d)", links.size());
+        for (Element link : links) {
+            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
+        }
+	}
+	private static void print(String msg, Object... args) {
+        System.out.println(String.format(msg, args));
+    }
+	private static String trim(String s, int width) {
+        if (s.length() > width)
+            return s.substring(0, width-1) + ".";
+        else
+            return s;
+    }
+}
