14596a0e8656c56a2ea9ca1bc0bea4e7847f02fe
Andre Karge <andre.karge@uni-weimar.de>
2014-09-02 15:18:01 +0200

    zaehlen der titel im artikel funktioniert und wird ins Pair gespeichert

--- src/wiki_parser/Pair.java
+++ src/wiki_parser/Pair.java
@@ -1,6 +1,7 @@
 package wiki_parser;
 
 public class Pair {
-	public String url;
-	public String titel;
+	public String url = "";
+	public String titel = "";
+	public int count = 0;
 }
--- src/wiki_parser/Parser.java
+++ src/wiki_parser/Parser.java
@@ -1,5 +1,11 @@
 package wiki_parser;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
@@ -9,9 +15,9 @@ import org.jsoup.select.Elements;
 
 public class Parser {
 	public static void main(String[] args) {
-		/*String testURL = "http://de.wikipedia.org/wiki/Bauhaus-Universit%C3%A4t_Weimar";
+		String testURL = "http://de.wikipedia.org/wiki/Bauhaus-Universit%C3%A4t_Weimar";
 		//String testURL = "http://de.wikipedia.org/wiki/Deutschland";
-		Elements elements = parse(testURL);
+		/*Elements elements = parse(testURL);
 		ArrayList<Pair> links = makeList(elements);
 		//printList(links);
 		System.out.println(links.size());
@@ -24,6 +30,9 @@ public class Parser {
 		links = clearDoubled(links);
 		System.out.println(links.size());
 		printList(links);*/
+		ArrayList<Pair> l = getList(testURL);
+		printList(l);
+		getHTMLText(testURL);
 	}
 	private static Elements parse(String inURL) {
 		String url = inURL;
@@ -47,14 +56,14 @@ public class Parser {
 		for(Element e : el) {
 			Pair tmp = new Pair();
 			tmp.url = e.attr("abs:href");
-			tmp.titel = trim(e.text(), 35);
+			tmp.titel = trim(e.text(), 100);
 			liste.add(tmp);
 		}
 		return liste;
 	}
 	private static void printList(ArrayList<Pair> l) {
 		for(Pair p : l) {
-			System.out.println(p.titel + " -- " + p.url);
+			System.out.println(p.titel + " -- " + p.url + "  --  " + p.count);
 		}
 	}
 	// Entfernt alle Links, die doppelt vorkommen
@@ -81,7 +90,8 @@ public class Parser {
 					|| p.url.startsWith("http://de.wikipedia.org/wiki/Wikipedia")
 					|| p.url.startsWith("http://de.wikipedia.org/wiki/Portal")
 					|| p.url.startsWith("http://de.wikipedia.org/wiki/Kategorie")
-					|| p.url.startsWith("http://de.wikipedia.org/wiki/Diskussion")) {
+					|| p.url.startsWith("http://de.wikipedia.org/wiki/Diskussion")
+					|| p.titel.length() < 3) {
 				tmp.add(p);
 			}
 		}
@@ -106,6 +116,33 @@ public class Parser {
 		links = clearNoArticle(links);
 		links = clearThisArticle(links, url);
 		links = clearDoubled(links);
+		String text = getHTMLText(url);
+		for(Pair p : links) {
+			p.count = countTitle(p.titel, text);
+		}
 		return links;
+	}
+	private static String getHTMLText(String inURL) {
+
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(inURL).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String text = doc.body().text();
+		return text;
+	}
+	private static int countTitle(String title, String site) {
+		int lastIndex = 0;
+		int count = 0;
+		while(lastIndex != -1) {
+			lastIndex = site.indexOf(title,lastIndex);
+			if(lastIndex != -1) {
+				count ++;
+				lastIndex += title.length();
+			}
+		}
+		return count;
 	}
 }
